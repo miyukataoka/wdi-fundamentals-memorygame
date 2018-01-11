58e975d4910df2ccb34cac11bcb5535a906fbbb0
Miyu Kataoka <miyukataoka@Miyus-MacBook-Air.local>
2018-01-10 21:20:22 -0800

    Unit 7 because I made a mistake previously in my github uploads. This is me fixing it.

---
+++ memory_game/Unit 7.js
@@ -0,0 +1,9 @@
+// Create variables for storing every card one by one
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+// console.log() to display two cards that user flipped
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
